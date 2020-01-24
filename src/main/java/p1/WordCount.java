710a09b63ed86a89e2c2f0954312914f4ed80e56
s.martinez <s.martinez@indizen.com>
2020-01-24 11:42:28 +0100

    ponemos clase principal

--- src/main/java/p1/WordCount.java
+++ src/main/java/p1/WordCount.java
@@ -1,11 +1,9 @@
-package p1;
+package main.java.p1;
 
-import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.operators.UnsortedGrouping;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 
