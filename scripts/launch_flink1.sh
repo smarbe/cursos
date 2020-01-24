3181f359735ec5733cc8f77cfc7180208663b3d9
s.martinez <s.martinez@indizen.com>
2020-01-24 13:57:47 +0100

    Introducción scripts para lanzamiento

--- scripts/launch_flink1.sh
+++ scripts/launch_flink1.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 PATHFLINK="/usr/flink/flink-1.9.1/bin/flink"
 PATHHOME="/home/flink/"
-PATHAPP= ${PATHHOME}"Projects/Cursos/"
-$PATHFLINK run ${PATHHOME}"bin/reduce-0.0.1-SNAPSHOT.jar --input " ${PATHHOME}"input/flink1.txt --output" ${PATHHOME}"output/flink1.txt"
+PATHAPP= ""${PATHHOME}Projects/Cursos/"
+
+NOW=$(date +"%YY%m%d%H%M%S")
+$PATHFLINK run ${PATHAPP}bin/reduce-0.0.1-SNAPSHOT.jar --input  ${PATHHOME}input/flink1.txt --output ${PATHHOME}output/flink1${NOW}.txt
